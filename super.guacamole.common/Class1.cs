3444ec2cc5c00171c71b9506f9c7ac70661b09ec
Joshua Winkler <josh@bemoty.dev>
2024-04-07 17:09:58 +0200

    Initial commit

---
+++ super.guacamole.common/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace super_guacamole_common;
+
+public class Class1
+{
+    
+}
---
+++ super.guacamole.web/Program.cs
@@ -0,0 +1,18 @@
+﻿using WatsonWebserver;
+using WatsonWebserver.Core;
+
+namespace super.guacamole.web;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Webserver server = new Webserver(new WebserverSettings("127.0.0.1", 3000), DefaultRoute);
+        server.Start();
+        Console.WriteLine("Listening on 127.0.0.1:3000");
+        Console.ReadLine();
+    }
+
+    static async Task DefaultRoute(HttpContextBase ctx) =>
+        await ctx.Response.Send("Hello from the default route!");
+}
